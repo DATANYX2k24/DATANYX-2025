dc09627c5b722eb74fdbfc02212368a6cb659927
Mohammed Osman <muhammedosmanraheemkhan@gmail.com>
2025-10-02 21:01:02 +0530

    Feat : Initialized Datanyx website with Next.js + TypeScript + bun setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Datanyx 2k25",
+  description: "Hackathon organised by SMC and AWS in colaboration",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
